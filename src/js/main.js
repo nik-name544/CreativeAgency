a5c7378eed4d69eb1779d5d26108ba7153ecb902
nik-name544 <revenkonik30@gmail.com>
2020-06-17 15:27:21 +0300

    slider-upd_v0.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
     $('.header__menu-btn').on('click', function () {
         $('.header__list').toggleClass("off");
+    });
+
+    $('.like-slider__inner').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
       });
 });
